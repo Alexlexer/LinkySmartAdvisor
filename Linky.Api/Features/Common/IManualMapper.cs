71db5e06b061b4e86e1b84c813b6fdb6517a05cc
AlexK <alex.khodyrev@hotmail.com>
2026-01-30 10:50:44 +0100

    chore: initialize production-ready structure with vsa and core packages

---
+++ Linky.Api/Features/Common/IManualMapper.cs
@@ -0,0 +1,9 @@
+﻿namespace Linky.Api.Features.Common;
+
+/// <summary>
+/// Manual mapping.
+/// </summary>
+public interface IManualMapper<in TSource, out TDestination>
+{
+    TDestination Map(TSource source);
+}
